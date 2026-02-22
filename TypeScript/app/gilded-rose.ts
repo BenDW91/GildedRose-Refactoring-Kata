3d1e92233f15955d5b8be2221c74363eafd82ad3
Ben De Weerdt <bendeweerdt@MacBook-Pro-van-Ben.local>
2026-02-22 16:08:12 +0100

    Made use of the constant file in the application to make it easier maintainable in the future

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,3 +1,5 @@
+import { ItemNames } from "./constants";
+
 export class Item {
   name: string;
   sellIn: number;
@@ -19,16 +21,16 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (this.items[i].name != ItemNames.AGED_BRIE && this.items[i].name != ItemNames.BACKSTAGE_PASS) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name != ItemNames.SULFURAS) {
             this.items[i].quality = this.items[i].quality - 1
           }
         }
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].name == ItemNames.BACKSTAGE_PASS) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1
@@ -42,19 +44,19 @@ export class GildedRose {
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name != ItemNames.SULFURAS) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].name != ItemNames.AGED_BRIE) {
+          if (this.items[i].name != ItemNames.BACKSTAGE_PASS) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name != ItemNames.SULFURAS) {
                 this.items[i].quality = this.items[i].quality - 1
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
+            this.items[i].quality = 0
           }
         } else {
           if (this.items[i].quality < 50) {
